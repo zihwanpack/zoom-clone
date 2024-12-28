7b46d1da19edfdec23eda65d6723bda7370ce75c
Layton <jihwan8807@naver.com>
2024-12-28 23:43:58 +0900

    feature : socket io sample

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert('hi');
+// io function이 알아서 socket.io를 실행하는 서버를 찾는다.
+const socket = io();
+const welcome = document.getElementById('welcome');
+const form = welcome.querySelector('form');
+
+function backendDone(msg) {
+  console.log(`backend say: ${msg}`);
+}
+
+const handleRoomSubmit = (event) => {
+  event.preventDefault();
+  const input = form.querySelector('input');
+  socket.emit('enter_room', input.value, backendDone);
+  input.value = '';
+};
+
+form.addEventListener('submit', handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import http from 'http';
+import SocketIO from 'socket.io';
 
 const app = express();
+
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/*', (req, res) => res.render('home'));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+wsServer.on('connection', (socket) => {
+  socket.on('enter_room', (roomName, done) => {
+    console.log(`Message: ${roomName}`);
+    // done을 실행하면 백이 아니라 프론트에서 함수가 실행된다. (실행의 주체는 백)
+    done('hello');
+  });
+});
+
+httpServer.listen(3000, handleListen);
